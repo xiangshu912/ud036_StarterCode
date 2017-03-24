6c49a8277729871985cc61cb03bcb453c7ce336c
Xiang Shu <Shawn@TMT.local>
2017-03-23 17:43:21 -0700

    update

--- media.py
+++ media.py
@@ -1,8 +1,6 @@
 
 class Movie(object):
-    """Movie in the entertainment center.
-
-    Attributes:
+    """
         title: Movie title.
         poster_image_url: URL to movie poster image.
         trailer_youtube_url: URL to movie trailer on YouTube.
@@ -10,7 +8,6 @@ class Movie(object):
     """
 
     def __init__(self, title, poster_image_url, trailer_youtube_url, director):
-        """Constructor."""
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
